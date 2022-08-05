31ed01667bfb5d674e35b8ce0fefbacda1b0352e
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-05 12:57:51 +0100

    set up port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
+const PORT = process.env.PORT || 4000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+const init = async () => {
+  const { url } = await server.listen({ port: PORT });
+  console.log(`🚀  Server ready at ${url}`);
+};
+
+init();
