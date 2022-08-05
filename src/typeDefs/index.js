2136a352861061adfe8d3e96143eb9982e0a4a23
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-05 12:16:10 +0100

    schema types

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,5 +1,19 @@
 const { gql } = require("apollo-server");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Book {
+    id: ID
+    title: String
+    authors: [String]
+    description: String
+    pageCount: Int
+    categories: [String]
+    averageRating: Float
+    isEbook: Boolean
+  }
+  type Query {
+    books(searchTerm: String): [Book]
+  }
+`;
 
 module.exports = typeDefs;
